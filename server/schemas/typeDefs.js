45b7c204b1dad7248fc09a8ce05ca05c66b4bb9e
Akeem C <javantieclarke@gmail.com>
2021-10-12 16:52:01 -0400

    added the user query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,36 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-type Thought {
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
   }
-  type Query {
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
     thoughts: [Thought]
+    friends: [User]
+  }
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
